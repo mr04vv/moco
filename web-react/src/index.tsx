25b5c729b41b5c3aeb780bee8b4793e06be4990c
Takuto Mori <takuto.mori@atrae.co.jp>
2021-04-24 20:42:10 +0900

    add: demoページ作成

---
+++ web-react/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const AA = () => {
+    return (<div>moritakutohagumpyo</div>)
+}
+  
+ReactDOM.render(<AA />, document.getElementById("root"));
+  
