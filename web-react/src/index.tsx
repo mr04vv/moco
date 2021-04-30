43d33669787c37dd4e018d069257924d138c817e
Takuto Mori <mr04vv@gmail.com>
2021-04-30 14:20:14 +0900

    chore: テストコミット

--- web-react/src/index.tsx
+++ web-react/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const AA = () => {
-  return <div>moritakutohagumpyo</div>;
+  return <div>moritakutohagumpy</div>;
 };
 
 ReactDOM.render(<AA />, document.getElementById('root'));
